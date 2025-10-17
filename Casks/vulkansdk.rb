b1d7efefe3d0dfa81004b058ffb9d7799a113d8a
nyanters <123231885+nyanters@users.noreply.github.com>
2025-10-17 21:39:58 +0900

    new file:   Casks/vulkansdk.rb

---
+++ Casks/vulkansdk.rb
@@ -0,0 +1,16 @@
+cask "vulkansdk" do
+  version "1.4.328.1"
+  sha256 "571db867d8736e402f6f674f26f17b339495d016f3b2afdbac23a3650475023f"
+
+  url "https://sdk.lunarg.com/sdk/download/#{version}/mac/vulkansdk-macos-#{version}.zip"
+  name "Vulkan SDK"
+  desc "cross-platform graphics and compute API"
+  homepage "https://vulkan.lunarg.com/sdk/home"
+
+  container type: :zip
+
+  app "vulkansdk-macOS-#{version}.app"
+
+  zap trash: [
+  ]
+end
