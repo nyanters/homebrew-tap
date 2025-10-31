eddb50a30c50d483e950db2a05c6bd89547f5594
nyanters <123231885+nyanters@users.noreply.github.com>
2025-10-31 17:37:56 +0900

    new file:   Casks/pcsx2.rb

---
+++ Casks/pcsx2.rb
@@ -0,0 +1,18 @@
+cask "pcsx2" do
+  version "2.4.0"
+  sha256 "9c4c4abb95b0055c5403c3f70d9ed25659f9cce457711de533f8168c5c60015f"
+
+  url "https://github.com/PCSX2/pcsx2/releases/download/v2.4.0/pcsx2-v#{version}-macos-Qt.tar.xz"
+  name "PCSX2"
+  desc "Playstation 2 Emulator"
+  homepage "https://github.com/PCSX2/pcsx2"
+
+  container type: :tar
+  rename "PCSX2-v#{version}.app" "PCSX2.app"
+  app "PCSX2.app"
+
+  zap trash: [
+    "~/Library/Application Support/PCSX2",
+    "~/Library/Preferences/net.pcsx2.pcsx2.plist",
+  ]
+end
