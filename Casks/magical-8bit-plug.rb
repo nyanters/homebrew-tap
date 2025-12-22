5c2552610063ce8dfc728901ee035b86bead9e00
lache-sys <program6493@outlook.com>
2025-12-22 17:23:14 +0900

    new file:   Casks/magical-8bit-plug.rb

---
+++ Casks/magical-8bit-plug.rb
@@ -0,0 +1,15 @@
+cask "magical-8bit-plug" do
+  version :latest
+  sha256 :no_check
+
+  url "https://ymck.net/data/magical8bitplug/Magical8bitPlug2-mac-241023.zip"
+  name "Magical 8bit Plug"
+  desc "Software-synthesizer generates primitive electronic sounds"
+  homepage "https://ymck.net/app/magical-8bit-plug-en"
+
+  container type: :zip
+
+  audio_unit_plugin "Component/Magical8bitPlug2.component"
+  vst_plugin "VST/Magical8bitPlug2.vst"
+  vst3_plugin "VST3/Magical8bitPlug2.vst3"
+end
